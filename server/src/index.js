390f3a9484592854dc163836cb2fcdfef1cba6e8
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2026-06-09 20:37:19 +0530

    add database connection logic and refactor server startup

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+connectDB().then(
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  }),
+);
