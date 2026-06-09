3ef9b21f6f2a451a3bbffca96a7cbe47210dddfb
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2026-06-09 20:24:49 +0530

    project initialized

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Api is working",
+  });
+});
+
+export default app;
