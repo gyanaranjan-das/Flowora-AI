744ba6b75de5d32fee63e73ac2e005e8bb2475d7
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2026-06-10 07:32:41 +0530

    add AI routes and user model schema

---
+++ server/src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "mandatory"],
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userModel);
+export default User;
---
+++ server/src/routes/ai.route.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const route = Router();
+
+route.post("/breakdown");
+route.post("/priority");
+route.post("/daily-summary");
